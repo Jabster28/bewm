360d32438e12f0a1eb8de443f60166d865eb09a5
Jabster28 <justynboyer@gmail.com>
2021-03-23 20:20:51 +0000

    chore: testing things

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::process::Command;
+
 fn main() {
     println!("BEWM!!!");
+    // #[cfg(feature = "reverse_shell")]
+    #[cfg(unix)]
+    println!("yay unix");
+
+    #[cfg(windows)]
+    println!("eww windows");
 }
